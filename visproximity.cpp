935ed3b9bc867c79f273f0b1113e970c311f2feb
Ali Jewers <amj523@york.ac.uk>
2017-04-27 11:21:14 +0100

    IR Data vis adjustments

--- visproximity.cpp
+++ visproximity.cpp
@@ -51,9 +51,9 @@ void VisProximity::render(cv::Mat image, RobotData *robot, bool selected) {
             int r = (int)((raw/4095.0) * 255);
             int s = (int)((raw/4095.0) * 2) + 2;
             cv::Point sens = cv::Point(x + (int)(20 * cos(a * PI/180)), y + (int)(20 * sin(a * PI/180)));
-            rectangle(image, sens - cv::Point(s, s), sens + cv::Point(s, s), cv::Scalar(0, 0, r), CV_FILLED);
+            rectangle(image, sens - cv::Point(s, s), sens + cv::Point(s, s), cv::Scalar(255, 255-r, 255-r), CV_FILLED);
         } else {
-            double len = square(((raw/4095.0) * 10.0) - 10.0);
+            double len = square(((raw/4095.0) * 10.0) - 10.0) * 0.4;
             cv::Point end = cv::Point(x + (int)(len * cos(a * PI/180)), y + (int)(len * sin(a * PI/180)));
             line(image, cv::Point(x, y), end, robot->getColour(), 1);
         }
