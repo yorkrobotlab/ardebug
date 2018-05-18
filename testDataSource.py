b3256ee76dd890f6f981b4744fdb3bdae0bec56a
Alan Millard <alan.millard@york.ac.uk>
2018-05-18 23:00:05 +0100

    Updated test script to optionally output robot poses

--- testDataSource.py
+++ testDataSource.py
@@ -4,61 +4,74 @@ import json
 import time
 import random
 
+import socket
+from time import sleep
+
+import sys
+
 class Robot:
 
-    def __init__(self, id):
+    def __init__(self, id, reportPose):
         self.id = id
         self.state = "IDLE"
         self.ir = [random.randint(0, 4095) for _ in range(8)]
         self.battery_voltage = random.uniform(4.0, 4.2)
         self.last_state_change = time.time() + random.uniform(0, 3)
         self.pose = {'x': random.uniform(0.05, 0.95), 'y': random.uniform(0.05, 0.95), 'orientation': random.uniform(0, 360)}
+        self.reportPose = reportPose
 
     def toJson(self):
-        return json.dumps({
+        values = {
                     'id': self.id,
                     'state': self.state,
                     'ir': self.ir,
-                    'battery_voltage': self.battery_voltage,
-                    'pose': self.pose
-               })
-
-robots = [Robot('robot_%d' % (i,)) for i in range(8)]
-    
-import socket
-from time import sleep
+                    'battery_voltage': self.battery_voltage
+               }
+        
+        if self.reportPose:
+            values['pose'] = self.pose
+            
+        return json.dumps(values)
 
 hostName = ''
 hostPort = 8888
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect((hostName, hostPort))
-
 states = ["IDLE", "WALKING", "TURNING", "AVOIDING"]
 
-while True:
-    for r in robots:
+if __name__=="__main__":
+    reportPose = True
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "aruco":
+            reportPose = False
+            
+    robots = [Robot('robot_%d' % (i,), reportPose) for i in range(8)]
 
-        if(time.time() - r.last_state_change > 5):
-            r.state = random.choice(states)
-            r.last_state_change = time.time()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((hostName, hostPort))
 
-        r.ir = [x + random.randint(-100, 100) for x in r.ir]
-        
-        r.ir = [0 if x < 0 else x for x in r.ir]
+    while True:
+        for r in robots:
 
-        if(r.battery_voltage > 0):
-            r.battery_voltage -= random.uniform(0, 0.001)
-            if(r.battery_voltage < 0):
-                r.battery_voltage = 0
+            if(time.time() - r.last_state_change > 5):
+                r.state = random.choice(states)
+                r.last_state_change = time.time()
 
-        print(r.toJson())
+            r.ir = [x + random.randint(-100, 100) for x in r.ir]
 
-        try:
-            s.send(r.toJson())
-        except:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect((hostName, hostPort))
-            s.send(r.toJson())
+            r.ir = [0 if x < 0 else x for x in r.ir]
 
-    sleep(0.1)
+            if(r.battery_voltage > 0):
+                r.battery_voltage -= random.uniform(0, 0.001)
+                if(r.battery_voltage < 0):
+                    r.battery_voltage = 0
+
+            print(r.toJson())
+
+            try:
+                s.send(r.toJson())
+            except:
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.connect((hostName, hostPort))
+                s.send(r.toJson())
+
+        sleep(0.1)
